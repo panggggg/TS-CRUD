df83478891bc9eaa46208134d74308a84b61978b
panggggg <p.picczy@gmail.com>
2021-12-16 17:55:23 +0700

    Update app.ts add mongodb connection and routes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import bodyParser from 'body-parser'
 import express, {Request, Response} from 'express'
 import mongoose from 'mongoose'
+import router from '../src/routes/book.route'
 
 const app = express()
 const port: number = 3000
 
+const username: string = 'root'
+const password: string = 'root'
+mongoose.connect(`mongodb://${username}:${password}@localhost:27017/books-api?authSource=admin`, (err) => {
+    if (err) throw err;
+    console.log('Connected to database')
+})
+
+app.use(bodyParser.json());
+
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello Pangpicc")
 })
+
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`App listening at port ${port}`)
