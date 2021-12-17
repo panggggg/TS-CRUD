03939eda3db6ae54a06126e2bb87283e51175694
kokkoks <kok.suttanan@gmail.com>
2021-12-17 12:42:36 +0700

    Update routes and controllers

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,23 +1,53 @@
-import bookModel, {Books} from "../models/book.model"
-import {Request, Response} from 'express'
+import bookModel, { Books } from "../models/book.model"
+import { Request, Response } from 'express'
 
 export let getAllBooks = async (req: Request, res: Response) => {
     await bookModel.find({}, (err: any, book: Books) => {
-        try{
-            res.status(200).send(book)
-        }catch(e: any){
-            res.status(404).send(e)
+        if (err) {
+            res.send(err)
         }
+        res.send(book)
+    })
+}
+
+export let getBookById = async (req: Request, res: Response) => {
+    let bookId: string = req.params.id
+    await bookModel.findById({ _id: bookId }, (err: any, book: Books) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send(book)
     })
 }
 
 export let createBook = async (req: Request, res: Response) => {
     const newBook = await new bookModel(req.body)
     newBook.save((err: any, book: Books) => {
-        try{
-            res.status(200).send({message: 'Successfully added'})
-        } catch(e: any){
+        try {
+            res.status(200).send({ message: 'Successfully added' })
+        } catch (e: any) {
             res.status(404).send(e.message)
         }
     })
 }
+
+export let updateBookById = async (req: Request, res: Response) => {
+    let bookId: string = req.params.id
+    await bookModel.findByIdAndUpdate({ _id: bookId }, (err: any, book: Books) => {
+        if (err) {
+            res.status(404).send(err)
+        }
+        res.status(200).send(`Book id ${bookId} has been updated!`)
+    })
+}
+
+export let deleteBookById = async (req: Request, res: Response) => {
+    let bookId: string = req.params.id
+    await bookModel.findByIdAndDelete({ _id: bookId }, (err: any, book: Books) => {
+        if (err) {
+            res.status(404).send(err)
+        }
+        res.status(200).send(`Book id ${bookId} has been deleted!`)
+    })
+}
+
--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -1,9 +1,12 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import * as booksController from '../controllers/book.controller'
 
 const router: Router = Router()
 
 router.route('/books').get(booksController.getAllBooks)
+router.route('/books/:id').get(booksController.getBookById)
 router.route('/books').post(booksController.createBook)
+router.route('/books/:id').patch(booksController.updateBookById)
+router.route('books/:id').delete(booksController.deleteBookById)
 
 export default router
