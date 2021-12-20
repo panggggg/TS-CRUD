c3f30b273b744eccf0156fb6650fe6d53b764916
panggggg <p.picczy@gmail.com>
2021-12-20 23:28:10 +0700

    Add default.ts as config file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import bodyParser from 'body-parser'
+import config from 'config'
 import express, {Request, Response} from 'express'
 import mongoose from 'mongoose'
 import router from '../src/routes/book.route'
 
-const app = express()
-const port: number = 3000
 
-const username: string = 'root'
-const password: string = 'root'
+const app = express()
+const port: number = config.get('port')
+
+const username: string = config.get("mongoUsername")
+const password: string = config.get('mongoPassword')
 mongoose.connect(`mongodb://${username}:${password}@localhost:27017/books-api?authSource=admin`, (err) => {
     if (err) throw err;
     console.log('Connected to database')
